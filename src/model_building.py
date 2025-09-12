ba4dbba6bb54924743f0bd17a51724e4e40febb5
odubajo <odubajoq@gmail.com>
2025-09-12 20:28:23 +0100

    hyperparameter tuning

--- src/model_building.py
+++ src/model_building.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import yaml
 
 import pickle
 
@@ -8,10 +9,13 @@ from sklearn.ensemble import RandomForestClassifier
 
 train_data = pd.read_csv("data/processed/train_processed.csv")
 
-X_train = train_data.drop(columns=['potability'],axis=1)
-y_train = train_data['potability']
+n_estimators = yaml.safe_load(open("params.yaml"))['model_building']['n_estimators']
+min_samples_split = yaml.safe_load(open("params.yaml"))['model_building']['min_samples_split']
 
-clf= RandomForestClassifier(random_state=42)
+y_train = train_data['Potability']
+X_train = train_data.drop(columns=['Potability'],axis=1)
+
+clf= RandomForestClassifier(n_estimators=n_estimators, min_samples_split=min_samples_split ,random_state=42)
 clf.fit(X_train, y_train)
 
 pickle.dump(clf,open("model.pkl","wb"))
